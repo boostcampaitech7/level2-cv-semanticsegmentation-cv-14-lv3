e2f4bacb554c6d63a5bd3db68aeabe8e6e7eda88
chaewon <mcw1156@gmail.com>
2024-11-13 21:35:02 +0900

    ✨ feat: Enhance wandb functionality
    - Add wandb experiment name configuration via parser
    - Implement class-wise loss and dice metric logging
    - Add worst samples visualization in Media tab
    
    Related to #8

--- visualize.py
+++ visualize.py
@@ -21,8 +21,29 @@ def label2rgb(label):
         
     return image
 
-def visualize_prediction(image, preds):
-    fig, ax = plt.subplots(1, 2, figsize=(24, 12))
-    ax[0].imshow(image)
-    ax[1].imshow(label2rgb(preds))
-    plt.show() 
+def visualize_prediction(image, pred_mask, true_mask=None):
+    """
+    예측 결과를 시각화하는 함수
+    
+    Args:
+        image: 원본 이미지 (H, W, 3)
+        pred_mask: 예측 마스크 (C, H, W)
+        true_mask: 실제 마스크 (C, H, W), 옵션
+    """
+    if true_mask is None:
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 12))
+        ax1.imshow(image)
+        ax1.set_title('Original Image')
+        ax2.imshow(label2rgb(pred_mask))
+        ax2.set_title('Prediction')
+    else:
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(36, 12))
+        ax1.imshow(image)
+        ax1.set_title('Original Image')
+        ax2.imshow(label2rgb(pred_mask))
+        ax2.set_title('Prediction')
+        ax3.imshow(label2rgb(true_mask))
+        ax3.set_title('Ground Truth')
+    
+    plt.tight_layout()
+    return fig
