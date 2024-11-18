85fa7f1441482c890e441245bb90b8374d56741f
chaewon <mcw1156@gmail.com>
2024-11-18 16:30:42 +0700

    🔥 Remove loss functions #29

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import wandb
 from dataset import XRayDataset, CLASSES
 import torch
 import torch.nn.functional as F
-from pytorch_msssim import ms_ssim
+from loss import get_loss 
 
 # U-Net3+
 from model import build_unet3plus, UNet3Plus
@@ -32,7 +32,7 @@ def parse_args():
                         help='이미지 Resize')
     parser.add_argument('--save_dir', type=str, default='./checkpoints',
                         help='모델 저장 경로')
-    parser.add_argument('--batch_size', type=int, default=4,
+    parser.add_argument('--batch_size', type=int, default=2,
                         help='배치 크기')
     parser.add_argument('--lr', type=float, default=1e-4,
                         help='학습률')
@@ -40,56 +40,10 @@ def parse_args():
                         help='총 에폭 수')
     parser.add_argument('--val_interval', type=int, default=1,
                         help='검증 주기')
-    parser.add_argument('--wandb_name', type=str, default='unet3p_1_test',
+    parser.add_argument('--wandb_name', type=str, default='loss_test',
                         help='wandb에 표시될 실험 이름')
 
     return parser.parse_args()
-
-def dice_loss(pred, target, smooth=1.):
-    pred = pred.contiguous()
-    target = target.contiguous()   
-    intersection = (pred * target).sum(dim=2).sum(dim=2)
-    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
-    return loss.mean()
-
-def IOU_loss(inputs, targets, smooth=1) :    
-    inputs = inputs.reshape(-1)
-    targets = targets.reshape(-1)
-    intersection = (inputs * targets).sum()
-    total = (inputs + targets).sum()
-    union = total - intersection 
-    IoU = (intersection + smooth)/(union + smooth)
-    return 1 - IoU
-
-def focal_loss(inputs, targets, alpha=0.25, gamma=2):
-    bce_loss = F.binary_cross_entropy(inputs, targets, reduction='none')  # (B, C, H, W)
-    pt = torch.where(targets == 1, inputs, 1 - inputs)  # (B, C, H, W)
-
-    focal = alpha * (1 - pt) ** gamma * bce_loss
-
-    return focal.mean()
-
-def msssim_loss(inputs, targets, data_range=1.0):
-    msssim_score = ms_ssim(inputs, targets, data_range=data_range, size_average=True)
-    
-    msssim_loss = 1 - msssim_score
-    return msssim_loss
-
-def calc_loss(pred, target, focal_weight=0.33, iou_weight=0.33, msssim_weight = 0.33):
-    
-    # focal Loss 계산
-    pred = F.sigmoid(pred)
-    focal = focal_loss(pred, target, alpha=0.25, gamma=2)
-    
-    # IoU Loss 계산
-    iou = IOU_loss(pred, target)
-
-    # msssim loss 계산
-    msssim = msssim_loss(pred, target)
-    
-    # 가중치 기반 결합
-    loss = focal_weight * focal + iou_weight * iou + msssim_weight * msssim
-    return loss
 
 def main():
     args = parse_args()
@@ -130,14 +84,13 @@ def main():
     )
 
     # 모델 설정
-    # model = models.segmentation.fcn_resnet50(pretrained=True)
     model = build_unet3plus(num_classes=29, encoder = 'resnet50', pretrained=True)
-    # model.classifier[4] = nn.Conv2d(512, len(CLASSES), kernel_size=1)
     model = model.cuda()
 
     # 손실 함수 및 옵티마이저 설정
-    criterion = calc_loss
+    criterion = get_loss('combined', weights={'bce': 0.5, 'dice': 0.5}) #bcediceloss를 base로 실험
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-6)
+
 
     # Wandb 초기화
     wandb.init(
