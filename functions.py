bd26fc77d04fbb88c3914d9a4e2413410e993bd0
namgyu-youn <yynk2012@gmail.com>
2024-11-20 20:56:10 +0900

    ⚡️ Improve decoder performance

--- functions.py
+++ functions.py
@@ -6,17 +6,17 @@ def dice_coef(y_true, y_pred):
     y_true_f = y_true.flatten(2)
     y_pred_f = y_pred.flatten(2)
     intersection = torch.sum(y_true_f * y_pred_f, -1)
-    
-    eps = 0.0001
-    return (2. * intersection + eps) / (torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps) 
 
-## RLE 
+    eps = 0.0001
+    return (2. * intersection + eps) / (torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps)
+
+## Mask -> RLE
 def encode_mask_to_rle(mask):
     '''
-    mask: numpy array binary mask 
-    1 - mask 
+    mask: numpy array binary mask
+    1 - mask
     0 - background
-    Returns encoded run length 
+    Returns encoded run length
     '''
     pixels = mask.flatten()
     pixels = np.concatenate([[0], pixels, [0]])
@@ -24,14 +24,16 @@ def encode_mask_to_rle(mask):
     runs[1::2] -= runs[::2]
     return ' '.join(str(x) for x in runs)
 
+# RLE -> Mask
 def decode_rle_to_mask(rle, height, width):
-    s = rle.split()
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
-    starts -= 1
+    s = np.array(rle.split(), dtype=int)
+    starts, lengths = s[0::2] - 1, s[1::2]
     ends = starts + lengths
-    img = np.zeros(height * width, dtype=np.uint8)
-    
-    for lo, hi in zip(starts, ends):
-        img[lo:hi] = 1
-    
-    return img.reshape(height, width) 
+
+    mask = np.zeros(height * width, dtype=np.int32)
+    mask[starts] += 1
+    mask[ends] -= 1
+
+    mask = np.cumsum(mask)
+
+    return mask.reshape(height, width).astype(np.uint8)
